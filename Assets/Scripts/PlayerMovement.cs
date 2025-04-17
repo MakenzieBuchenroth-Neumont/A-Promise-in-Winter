2214759fd8e034c15650bfa52eb11b42e7b45a9c
MakenzieBuchenroth-Neumont <mbuchenroth@student.neumont.edu>
2025-04-17 13:24:24 -0600

    Initial commit.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float moveSpeed;
+	private bool isMoving;
+	private Vector2 input;
+	private Animator animator;
+	public LayerMask solidObjectLayer;
+	public LayerMask interactableLayer;
+
+	private void Awake() {
+		animator = GetComponent<Animator>();
+	}
+
+	private void Update() {
+		if (!isMoving) {
+			input.x = Input.GetAxisRaw("Horizontal");
+			input.y = Input.GetAxisRaw("Vertical");
+
+			if (input.x != 0) input.y = 0;
+			if (input != Vector2.zero) {
+
+				animator.SetFloat("horizontal", input.x);
+				animator.SetFloat("vertical", input.y);
+
+				Vector3 targetPos = transform.position;
+				targetPos.x += input.x * moveSpeed * Time.deltaTime;
+				targetPos.y += input.y * moveSpeed * Time.deltaTime;
+
+				if (isWalkable(targetPos))
+					StartCoroutine(Move(targetPos));
+			}
+		}
+		animator.SetBool("isMoving", isMoving);
+
+		if (Input.GetMouseButtonDown(1)) {
+			interact();
+		}
+	}
+
+	private void interact() {
+		var facingDir = new Vector3(animator.GetFloat("horizontal"), animator.GetFloat("vertical"));
+		var interactPos = transform.position + facingDir;
+
+		Debug.DrawLine(transform.position, interactPos, Color.red, 1f);
+
+		var collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactableLayer);
+		if (collider != null) {
+			Debug.Log("There is an NPC.");
+		}
+	}
+
+	IEnumerator Move(Vector3 targetPos) {
+		isMoving = true;
+		while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon) {
+			transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+			yield return null;
+		}
+		transform.position = targetPos;
+		isMoving = false;
+	}
+
+	private bool isWalkable(Vector3 targetPos) {
+		if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectLayer | interactableLayer) != null) {
+			return false;
+		}
+		return true;
+	}
+}
